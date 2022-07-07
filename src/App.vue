e3070c24d89a478c10b3f799c34de680dbed2977
ADKcode <790532173@qq.com>
2022-07-07 22:12:53 +0800

    docs: ✏️ 更新示例

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,44 @@
 <script setup lang="ts">
 import V3Emoji from './components/V3Emoji.vue';
 const inputText = ref('');
-const appendText = (val: string) => {
-  inputText.value += val;
+const clickEmoji = (val: any) => {
+  clickEvent.value = val;
+  console.log(val);
 };
 const optionsName = {
   'Smileys & Emotion': '笑脸&表情',
   'Food & Drink': '食物&饮料'
 };
 const disableGroup = ['食物&饮料'];
+const abc = ref('这里是双向绑定的值');
+const clickEvent = ref('');
 </script>
 
 <template>
   <div id="app">
-    <div style="margin-top: 500px; display: flex; flex-direction: column; align-items: center">
-      <V3Emoji
-        :disable-group="disableGroup"
-        @click-emoji="appendText"
-        :options-name="optionsName"
-        :recent="true"
-      />
-      <p>点击我</p>
-      <input v-model="inputText" type="textarea" style="width: 300px" />
+    <div>
+      <h1>输入你的emoji</h1>
+      <div class="text-area">
+        <V3Emoji
+          :disable-group="disableGroup"
+          @click-emoji="clickEmoji"
+          :options-name="optionsName"
+          :recent="true"
+          default-select="Animals & Nature"
+          :fulldata="true"
+          :textArea="true"
+          :keep="false"
+          size="big"
+          :customSize="{ width: 500, height: 300 }"
+          v-model="abc"
+        />
+      </div>
+      <input style="width: 100%; margin-top: 10px" v-model="abc" />
+      <button @click="abc = ''">清空</button>
     </div>
+    <h2>你当前点击的是</h2>
+    {{ clickEvent }}
   </div>
-  <V3Emoji
-    :disable-group="disableGroup"
-    @click-emoji="appendText"
-    :options-name="optionsName"
-    :recent="true"
-  />
 </template>
 
 <style scoped>
@@ -39,5 +48,9 @@ const disableGroup = ['食物&饮料'];
   justify-content: center;
   align-items: center;
   flex-direction: column;
+}
+.text-area {
+  width: 800px;
+  height: 300px;
 }
 </style>
